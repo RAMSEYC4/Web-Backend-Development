7af648da7a21884d0153b24f19dc9e1923471296
RAMSEYC4 <ramseycedou68@gmail.com>
2026-07-01 20:54:57 +0200

    july 1 week 1 site

---
+++ server.js
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Tell Express where to find your templates
+app.set("views", path.join(__dirname, "src/views"));
+
+//server static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  const title = "Home";
+  res.render("home", { title });
+});
+
+app.get("/organizations", (req, res) => {
+  const title = "Our Partner Organization";
+  res.render("organizations", { title });
+});
+
+app.get("/projects", (req, res) => {
+  const title = "Service Projects";
+  res.render("projects", { title });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
